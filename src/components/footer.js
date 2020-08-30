ddbb391041f12f655a561fd4f40a1519ccc955fc
dsmoby <dsmoby@gmail>
2020-08-30 04:39:50 +0200

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer(params) {
+  return (
+    <footer>
+      <p>This is our footer</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/maincontent.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function MainContent() {
+  return <h1>Here is my main content</h1>
+}
+
+export default MainContent
